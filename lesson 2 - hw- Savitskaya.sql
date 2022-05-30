98ea7ae8a1c2f97df9321b1aec5e9a75f6708a53
savitskaya-om <106524258+savitskaya-om@users.noreply.github.com>
2022-05-30 20:12:48 +0300

    Delete lesson 2 - hw- Savitskaya.sql

--- lesson 2 - hw- Savitskaya.sql
+++
@@ -1,9 +0,0 @@
-select name, class from ships 
-where launched>1920
-
-select name, class from ships 
-where launched between 1920 and 1942 
-
-select class, count(name) as q from ships
-group by class
-
